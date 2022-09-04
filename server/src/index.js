0d487d528545fa53b022bdbc954673421dec4249
Harsh Keshri <harshkeshri1234567@gmail.com>
2022-09-04 23:37:29 +0530

    testing

--- server/src/index.js
+++ server/src/index.js
@@ -8,18 +8,6 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-// app.use(Router);
-
-// app.use("*", (req, res, next) => {
-//   res.send("404 Not Found");
-// });
-
-// DbConnect();
-
 app.get("/", (req, res) => {
   res.send("HELLO");
 });
